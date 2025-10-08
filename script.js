c3d0d7edd9a703e34afbfd5c870df2fb3b6a5526
strawberryshorcake <cunha.esthefany@escola.pr.gov.br>
2025-10-08 07:58:03 -0300

    salvar

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave() { 
-  alert("fui clicado!"); 
-} 
+function mostraPalavrasChave() {
+  const texto = document.querySelector("#entrada-de-texto").value;
+
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavras = texto.split(" ");
+
+  campoResultado.textContent = palavras.join(', ')
+}
