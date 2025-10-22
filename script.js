a0644830fbee4dfe33dad2ef8affdfaec48d92ca
strawberryshorcake <cunha.esthefany@escola.pr.gov.br>
2025-10-22 07:52:25 -0300

    salvar

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ function mostraPalavrasChave() {
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
 
-  let frequencias = [];
+  let frequencias = {};
 
-  for (let i in palavras) {
+  for (let i of palavras) {
     frequencias[i] = 0;
 
-    for (let j in palavras) {
-      if (palavras[i] == palavras[j]) {
+    for (let j of palavras) {
+      if (i == j) {
         frequencias[i]++;
       }
     }
